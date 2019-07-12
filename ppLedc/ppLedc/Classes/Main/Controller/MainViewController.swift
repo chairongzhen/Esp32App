bcf5f3ea9dc318cfccdaf051bf0e6d4f4ced59b3
Eric.Chai <241498527@qq.com>
2019-07-12 23:23:52 +0800

    modify folder structor

---
+++ ppLedc/ppLedc/Classes/Main/Controller/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  ppLedc
+//
+//  Created by 柴荣臻 on 2019/7/12.
+//  Copyright © 2019 rzchai. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addChildVc(storyName: "Basic")
+        addChildVc(storyName: "Repeat")
+        addChildVc(storyName: "Mine")
+        addChildVc(storyName: "Setting")
+    }
+    
+    private func addChildVc(storyName: String) {
+        let childVc = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        addChild(childVc)
+    }
+}
